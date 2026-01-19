e8a5aaedf3eaea19dceb4ac57fdfc85914b90ead
23f1002103 <23f1002103@ds.study.iitm.ac.in>
2026-01-20 00:38:34 +0530

    Use environment variables for email configuration

--- Web_App/Backend/celery/mail.py
+++ Web_App/Backend/celery/mail.py
@@ -22,15 +22,15 @@
 #     client.send_message(msg)
 #     client.quit()
 
-
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-SENDER_EMAIL = "noreply.fixify.com@gmail.com"  # Replace with your Gmail
-SENDER_PASSWORD = "pkvo qeio izlf npqa"  # Use App Password if 2FA is enabled
+SMTP_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("MAIL_PORT", 587))
+SENDER_EMAIL = os.getenv("MAIL_USERNAME")
+SENDER_PASSWORD = os.getenv("MAIL_PASSWORD")
 
 def send_mail(to, subject, content):
     msg = MIMEMultipart()
