effb58308c93be760b92adc0ac4da4653f9c5314
23f1002103 <23f1002103@ds.study.iitm.ac.in>
2026-01-20 16:50:47 +0530

    Fix config to support Railway environment

--- Web_App/Backend/config.py
+++ Web_App/Backend/config.py
@@ -1,11 +1,16 @@
+import os
+
 class Config:
-    SECRET_KEY = "my_secret_key"
-    JWT_SECRET_KEY = "super_secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///house_hold_service.sqlite"
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super_secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI",
+        "sqlite:///house_hold_service.sqlite"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access"]
-    
+
     # # Mail Configuration
     # MAIL_SERVER = "smtp.gmail.com"
     # MAIL_PORT = 465
